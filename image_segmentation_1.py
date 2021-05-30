3fd12a3ae561a737a10ac481940b7bb5d9647fd6
Maria <honey.shamonova@list.ru>
2021-05-30 21:02:37 +0300

    Сделала метрики

--- image_segmentation_1.py
+++ image_segmentation_1.py
@@ -66,7 +66,6 @@ def plantSeed(image):
         cv2.circle(image, (x , y), radius, color, thickness)
         cv2.circle(seeds, (x // SF, y // SF), radius // SF, code, thickness)
 
-
     def onMouse(event, x, y, flags, pixelType):
         global drawing
         if event == cv2.EVENT_LBUTTONDOWN:
@@ -82,7 +81,7 @@ def plantSeed(image):
         global drawing
         drawing = False
         windowname = "Plant " + pixelType + " seeds"
-        cv2.namedWindow(windowname, cv2.WINDOW_NORMAL)
+        cv2.namedWindow(windowname, cv2.WINDOW_AUTOSIZE)
         cv2.setMouseCallback(windowname, onMouse, pixelType)
         while (1):
             cv2.imshow(windowname, image)
@@ -108,52 +107,6 @@ def plantSeed(image):
     count_of_seeds = sum((int(intervals[i, 0]) for i in range(0, int(len(intervals)))))
 
     return seeds, image, intervals, count_of_seeds
-
-def addedSeed(seeds, image):
-    
-    def drawLines(x, y, pixelType):
-    
-        if pixelType == OBJ:
-            color, code = OBJCOLOR, OBJCODE
-        else:
-            color, code = BKGCOLOR, BKGCODE
-
-        cv2.circle(image, (x, y), radius, color, thickness)
-        cv2.circle(seeds, (x // SF, y // SF), radius // SF, code, thickness)
-            
-
-    def onMouse(event, x, y, flags, pixelType):
-        global drawing
-        if event == cv2.EVENT_LBUTTONDOWN:
-            drawing = True
-            drawLines(x, y, pixelType)
-        elif event == cv2.EVENT_MOUSEMOVE and drawing:
-            drawLines(x, y, pixelType)
-        elif event == cv2.EVENT_LBUTTONUP:
-            drawing = False
-
-    def paintSeeds(pixelType):
-        print("Planting", pixelType, "seeds")
-        global drawing
-        drawing = False
-        windowname = "Plant " + pixelType + " seeds"
-        cv2.namedWindow(windowname, cv2.WINDOW_AUTOSIZE)
-        cv2.setMouseCallback(windowname, onMouse, pixelType)
-        while (1):
-            cv2.imshow(windowname, image)
-            if cv2.waitKey(1) & 0xFF == 27:
-                break
-        cv2.destroyAllWindows()
-
-    radius = 10
-    thickness = -1 # fill the whole circle
-    global drawing
-    drawing = False
-
-    paintSeeds(OBJ)
-    paintSeeds(BKG)
-    
-    return seeds
     
 # Large when ip - iq < sigma, and small otherwise
 def boundaryPenalty(ip, iq):
@@ -195,32 +148,17 @@ def makeNLinks(graph, image):
     for i in range(r):
         for j in range(c):
             x = i * c + j + 1
-            if i + 1 < r: # pixel below
+            if i + 1 < r:
                 y = (i + 1) * c + j + 1
                 bp = boundaryPenalty(image[i][j], image[i + 1][j])
                 graph.add_edge(x, y, capacity=bp)
-                graph.add_edge(y, x, capacity=bp)
-                
+                graph.add_edge(y, x, capacity=bp) 
                 K = max(K, bp)
-            #if i + 1 < r: # pixel top
-            #    y = (i - 1) * c + j + 1
-            #    bp = boundaryPenalty(image[i][j], image[i - 1][j])
-            #    graph.add_edge(x, y, capacity=bp)
-            #    graph.add_edge(y, x, capacity=bp)
-                
-                K = max(K, bp)
-            if j + 1 < c: # pixel to the right
+            if j + 1 < c: 
                 y = i * c + j + 2
                 bp = boundaryPenalty(image[i][j], image[i][j + 1])
                 graph.add_edge(x, y, capacity=bp)
-                graph.add_edge(y, x, capacity=bp)
-                K = max(K, bp)
-            #if i + 1 < r: # pixel left
-            #    y = i * c + j 
-            #    bp = boundaryPenalty(image[i][j], image[i - 1][j])
-            #    graph.add_edge(x, y, capacity=bp)
-            #    graph.add_edge(y, x, capacity=bp)
-                
+                graph.add_edge(y, x, capacity=bp)           
                 K = max(K, bp)
     return K
 
@@ -241,7 +179,7 @@ def makeTLinks(graph, seeds, K, intervals, count_of_seeds, image):
                     x, 
                     capacity=regionalPenalty(
                         intervals[getInterval(image, i, j), 2],
-                        intervals[getInterval(image, i, j), 0]
+                        count_of_seeds
                     )
                 )  
                 graph.add_edge(
@@ -249,7 +187,7 @@ def makeTLinks(graph, seeds, K, intervals, count_of_seeds, image):
                     SINK, 
                     capacity=regionalPenalty(
                         intervals[getInterval(image, i, j), 1],
-                        intervals[getInterval(image, i, j), 0]
+                        count_of_seeds
                     )
                 ) 
               
@@ -267,16 +205,13 @@ def displayCut(image, cuts):
 
     for c in cuts:
         if c[0] != SOURCE and c[0] != SINK and c[1] != SOURCE and c[1] != SINK:
-          
             colorPixel(((c[0] - 1) // col) , ((c[0] - 1) % col))
-            
             colorPixel((c[1] - 1) // col, (c[1] - 1) % col)
     return image
 
 def createHistogram(imagefile):
     import cv2
     # импорт библиотеки для черчения
-    
     from matplotlib import pyplot as plt
      
     # читает входное изображение
@@ -299,24 +234,18 @@ def addedSeeds(image, graph, K, intervals, imagefile):
             if (graph.has_edge(SOURCE, vertex) and graph.has_edge(vertex, SINK)):
           
                 color, code = OBJCOLOR, OBJCODE
-          
-                edge_s_v = graph.edges[SOURCE, vertex]["capacity"]
-                edge_v_t = graph.edges[vertex, SINK]["capacity"]
                 
-                graph.edges[SOURCE, vertex]["capacity"] = edge_s_v + K + regionalPenalty(intervals[getInterval(image_gray, y, x), 1], allValue)
-                graph.edges[vertex, SINK]["capacity"] = edge_v_t + regionalPenalty(intervals[getInterval(image_gray, y, x), 2], allValue)
+                graph.edges[SOURCE, vertex]["capacity"] +=  K + regionalPenalty(intervals[getInterval(image_gray, y, x), 1], allValue)
+                graph.edges[vertex, SINK]["capacity"] += regionalPenalty(intervals[getInterval(image_gray, y, x), 2], allValue)
          
                 cv2.circle(image, (x, y), radius, color, thickness)
         else:  
             if (graph.has_edge(SOURCE, vertex) and graph.has_edge(vertex, SINK)):
           
                 color, code = BKGCOLOR, BKGCODE
-                
-                edge_s_v = graph.edges[SOURCE, vertex]["capacity"]
-                edge_v_t = graph.edges[vertex, SINK]["capacity"]
-                
-                graph.edges[SOURCE, vertex]["capacity"] = edge_s_v + K + regionalPenalty(intervals[getInterval(image_gray, y, x), 2], allValue)
-                graph.edges[vertex, SINK]["capacity"] = edge_v_t + regionalPenalty(intervals[getInterval(image_gray, y, x), 1], allValue)
+
+                graph.edges[SOURCE, vertex]["capacity"] +=  K + regionalPenalty(intervals[getInterval(image_gray, y, x), 2], allValue)
+                graph.edges[vertex, SINK]["capacity"] += regionalPenalty(intervals[getInterval(image_gray, y, x), 1], allValue)
                 
                 cv2.circle(image, (x, y), radius, color, thickness)
         
@@ -354,14 +283,50 @@ def addedSeeds(image, graph, K, intervals, imagefile):
     
     return graph
     
+def drawContur(image, reachable, non_reachable):
+    def colorPixel(i, j, color):
+        try:
+            image[i][j] = color
+        except:   
+            print(image[i][j])
+
+    col = len(image[0])
+    #image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+
+    for c in reachable:
+        if (c != SOURCE  and c != SINK):  
+            colorPixel(((c - 1) // col) , ((c - 1) % col), 255)
+       
+    for c in non_reachable:
+        if (c != SOURCE and c != SINK ):  
+            colorPixel(((c - 1 )// col) , ((c - 1 ) % col), 0)
+    
+    return image
+    
+def compareImages(image, image_compare):
+    
+    r, c = len(image_compare), len(image_compare[0])
+    size = (c, r)
+    image = cv2.resize(image, size)
+
+    correctly = 0
+    for i in range(r):
+        for j in range(c):
+            if (image[i][j] == image_compare[i][j]):
+                correctly += 1
+    relationship = correctly / ( r * c) 
+    print('relationship: ', relationship)
+    measure_zhakkar = correctly / r *c * 2
+    return relationship, measure_zhakkar
+      
 def imageSegmentation( ):
-    #size=(30, 30)
-    imagefile= 'imagecut.jpg'
+
+    imagefile= 'banana1-gr.jpg'
+    imagefile_compare = 'banana3-320.jpg'
     
     pathname = os.path.splitext(imagefile)[0]
     image = cv2.imread(imagefile, cv2.IMREAD_GRAYSCALE)
-    
-    #image = cv2.resize(image, size)
+    image_compare = cv2.imread(imagefile_compare, cv2.IMREAD_GRAYSCALE)
     
     graph, seededImage, K, seeds, intervals = buildGraph(image)
     createHistogram(imagefile)
@@ -381,9 +346,18 @@ def imageSegmentation( ):
     [('c', 'y'), ('x', 'b')]
     cut_value == sum(graph.edges[u, v]["capacity"] for (u, v) in cutset)
   
+    #Для того чтобы просто показать разбиение
     image = displayCut(image, cutset)
     
+    #Для того чтобы получить метрику разбиения
+    #image = drawContur(image,reachable, non_reachable)
+    
     show_image(image)
+    
+    #relationship, measure_zhakkar = compareImages(image, image_compare)
+    #print('1 metrica: ', relationship)
+    #print('2 metrica: ', measure_zhakkar)
+    
     savename = pathname + "cut.jpg"
     cv2.imwrite(savename, image)
     
